5e998e991494b1044d673fdea37e1fea492c5cac
enkhjino <enkhjin.ochirbal1@gmail.com>
2022-01-05 00:31:28 -0800

    mismatches count functionality and assets folder

--- js/main.js
+++ js/main.js
@@ -1,24 +1,26 @@
 /*----- constants -----*/
 const SOURCE_CARDS = [
-    { img: 'https://i.imgur.com/egWiMRQ.jpg', matched: true }
-    { img: 'https://i.imgur.com/yDGCwMt.jpg', matched: false }
-    { img: 'https://i.imgur.com/T0SVljJ.jpg', matched: false }
-    { img: 'https://i.imgur.com/gQ6fXZq.jpg', matched: false }
-    { img: 'https://i.imgur.com/vGIv57s.jpg', matched: false }
-    { img: 'https://i.imgur.com/z4Vtpeb.jpg', matched: false }
-    { img: 'https://i.imgur.com/0zlibP3.jpg', matched: false }
-    { img: 'https://i.imgur.com/DdcQ8L9.jpg', matched: false }
-    { img: 'https://i.imgur.com/qA5GhRB.png', matched: false }
-    { img: 'https://i.imgur.com/I7Y2yAW.png', matched: false }
-    { img: 'https://i.imgur.com/je3R2Ty.png', matched: false }
+    { img: 'https://i.imgur.com/egWiMRQ.jpg', matched: false },
+    { img: 'https://i.imgur.com/yDGCwMt.jpg', matched: false },
+    { img: 'https://i.imgur.com/T0SVljJ.jpg', matched: false },
+    { img: 'https://i.imgur.com/gQ6fXZq.jpg', matched: false },
+    { img: 'https://i.imgur.com/vGIv57s.jpg', matched: false },
+    { img: 'https://i.imgur.com/z4Vtpeb.jpg', matched: false },
+    { img: 'https://i.imgur.com/0zlibP3.jpg', matched: false },
+    { img: 'https://i.imgur.com/DdcQ8L9.jpg', matched: false },
+    { img: 'https://i.imgur.com/qA5GhRB.png', matched: false },
+    { img: 'https://i.imgur.com/I7Y2yAW.png', matched: false },
+    { img: 'https://i.imgur.com/je3R2Ty.png', matched: false },
     { img: 'https://i.imgur.com/cRaFvbr.png', matched: false }
 ]
 //6
 const CARD_BACK = "file:///Users/enjimenji/Downloads/monica-garniga-XClNDg9Z9Ag-unsplash.jpg";
+const SECOND_CARD_TIME = 3000;
 /*----- app's state (variables) -----*/
 let cards;//cards array of obj x2
 let selectedCard;
 let badGuessCount;
+let ignoreClick;
 //let turn; //turn variable to remember whose turn (1, -1)
 //let winner; //winner variable represents which player won, a tie, a game in play.null -> game in progress; 1/-1 a player has won; 'T' -> Tie
 //let playerScores;//player scores to be updated after every pick as well as using to determine winner
@@ -27,13 +29,13 @@ let badGuessCount;
 /*----- cached element references -----*/
 //7
 const btnEl = document.querySelector('button');
-//const msgEl = document.getElementById('msgEl');
-const cardEl = [...document.getElementById('cards')];
+const msgEl = document.getElementById('msgEl');
+const cardImgEls = document.querySelectorAll('#cards > img');
 
 
 /*----- event listeners -----*/
 btnEl.addEventListener('click', init);
-document.getElementById('cards').addEventListener('click', handleClick);
+document.getElementById('cards').addEventListener('click', handleChoice);
 
 
 /*----- functions -----*/
@@ -45,6 +47,7 @@ function init() {
     buildShuffledCards();
     selectedCard = null;
     badGuessCount = 0;
+    ignoreClick = false;
     render();
 }
 
@@ -52,11 +55,26 @@ function init() {
 function handleChoice(evt) {
     const cardIdx = parseInt(evt.target.id);
     const card = cards[cardIdx];
-    if (isNaN(cardIdx) || card.matched) return;
+    if (ignoreClick || isNaN(cardIdx) || card.matched) return;
     if (selectedCard && selectedCard === card) {
         badGuessCount++;
+        selectedCard = null;
     } else if (selectedCard) {
         //check for match
+        if (card.img === selectedCard.img) {
+            card.matched = selectedCard.matched = true;
+            selectedCard = null;
+        } else {
+            ignoreClick = true;
+            badGuessCount++;
+            card.matched = true;
+            setTimeout(function () {
+                ignoreClick = false;
+                selectedCard = null;
+                card.matched = false;
+                render();
+            }, SECOND_CARD_TIME);
+        }
     } else {
         selectedCard = card;
     }
@@ -79,13 +97,12 @@ function buildShuffledCards() {
 
 //5
 function render() {
-    let cardsHtml = '';
-    let curIdx = 0;
-    cards.forEach(function (card) {
+    //let cardsHtml = '';
+    //let curIdx = 0;
+    cards.forEach(function (card, idx) {
         const src = card.matched || selectedCard === card ? card.img : CARD_BACK;
-        cardsHtml += `<img src="${src} id="${curIdx}>`;
-        curIdx++;
+        cardImgEls[idx].src = src;
     });
     //8
-    cardEl.innerHTML = cardsHtml;
+    msgEl.innerText = `Mismatches: ${badGuessCount}`;
 }
